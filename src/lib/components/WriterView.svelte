752ce7f0924cc989c45cf1ba1bcf12266e77b9cb
aditya-xq <adityavivek1998@gmail.com>
2025-11-20 00:05:27 +0530

    feat: add a writer view

---
+++ src/lib/components/WriterView.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { loadWriteups } from '$lib/utils/stores'
+    import { onMount } from 'svelte'
+    import { appState } from '$lib/state.svelte'
+    import { Editor } from '.'
+
+    onMount(() => {
+        loadWriteups()
+    })
+
+    let content = $state('')
+
+    const handleInputChange = (markdown: string) => {
+        content = markdown
+    }
+
+    let quickPanelPadding = $derived(appState?.keepQuickPanelOpen ? 'lg:pl-56' : '')
+</script>
+
+<div class={`container mx-auto p-4 transition-all duration-300 ease-in-out ${quickPanelPadding}`}>
+    <div>
+        <Editor onInputChange={handleInputChange} />
+    </div>
+</div>
